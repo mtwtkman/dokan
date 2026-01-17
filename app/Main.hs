c4ad9018438d3aa56106afa1c12c873d2b2b366e
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2026-01-18 08:52:49 +0900

    added command line argumnet

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,20 @@
 module Main where
 
-import Dokan.Config (loadConfig)
 import Control.Monad.Except (runExceptT)
+import Data.Functor ((<&>))
+import Dokan.Config (loadConfig)
 import Dokan.Listener.Http (runHttp)
+import System.Environment (getArgs)
+
+defaultRouteMap :: FilePath
+defaultRouteMap = "dokan.yaml"
 
 main :: IO ()
 main = do
-  result <- runExceptT (loadConfig "example.yaml")
-  case result  of
-    Right routing -> runHttp routing
+  configName <- getArgs <&> \a -> if not (null a) then head a else defaultRouteMap
+  result <- runExceptT (loadConfig configName)
+  case result of
+    Right routing -> do
+      putStrLn $ "Proxy with " <> configName
+      runHttp routing
     Left e -> print e
-
